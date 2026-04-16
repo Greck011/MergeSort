1be0564f774e897ea6071199206b459cb531aaa5
Greck011 <71396160+Greck011@users.noreply.github.com>
2026-04-15 21:15:06 -0600

    Add files via upload

---
+++ MergeSort.java
@@ -0,0 +1,43 @@
+public class MergeSort {
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++) L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) arr[k++] = L[i++];
+            else arr[k++] = R[j++];
+        }
+        while (i < n1) arr[k++] = L[i++];
+        while (j < n2) arr[k++] = R[j++];
+    }
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {45, 12, 78, 3, 56, 89, 23, 67, 34, 90,
+                     11, 55, 42, 76, 8, 61, 29, 84, 17, 50};
+
+        System.out.println("Arreglo original:");
+        for (int num : arr) System.out.print(num + " ");
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.println("\nArreglo ordenado:");
+        for (int num : arr) System.out.print(num + " ");
+    }
+}
